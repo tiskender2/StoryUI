3fa09d4d1b69320c9c231612600e57f21b8afc8c
tolga <tolga.iskender@hepsiburada.com>
2022-04-28 17:47:37 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Tolga İskender on 28.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/StoryUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryUIApp.swift
+//  Shared
+//
+//  Created by Tolga İskender on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoryUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
