e191dcb025980c03b5ab4a0e54223717c6893ab8
tolga <tolga.iskender@hepsiburada.com>
2022-05-03 01:58:03 +0300

    bugfix for stopVideo

--- Sources/StoryUI/View/StoryView.swift
+++ Sources/StoryUI/View/StoryView.swift
@@ -41,6 +41,9 @@ public struct StoryView: View {
             .onAppear() {
                 startStory()
             }
+            .onDisappear() {
+               stopVideo()
+            }
         }
     }
     
@@ -48,6 +51,10 @@ public struct StoryView: View {
         storyData.stories = stories
         storyData.stories[selectedIndex].isSeen = true
         storyData.currentStoryUser = stories[selectedIndex].id
+    }
+    
+    private func stopVideo() {
+        NotificationCenter.default.post(name: .stopVideo, object: nil)
     }
 }
 
